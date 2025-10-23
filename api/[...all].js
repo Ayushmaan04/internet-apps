f2bdd0cc1a94630b156972c597bcfe4418f11e0d
Ayushmaan04 <yadavay@tcd.ie>
2025-10-23 23:38:42 +0100

    v fix

--- api/[...all].js
+++ api/[...all].js
@@ -1,3 +1,7 @@
+// api/[...all].js
 import serverless from "serverless-http";
 import app from "../bk-server.js";
+
+// This makes all /api/* requests go to your Express app,
+// and strips the /api prefix before Express sees the path.
 export default serverless(app, { basePath: "/api" });
